253c7684d1afe3e264f25535a044a0b2d2c5e9d0
Vadym Hotko <poipooo600@gmail.com>
2024-03-18 14:37:16 +0000

    added author and comments to post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView,DetailView
 from . import models
@@ -9,3 +10,27 @@ class IndexView(ListView):
     template_name = "blog/post_list.html"
     model = models.Post
     context_object_name = "posts"
+    
+class PostView(DetailView):
+    template_name = "blog/post_detail.html"
+    model = models.Post
+    context_object_name = "post"
+    
+
+        
+        
+    
+# render(request,'html',context)
+
+
+class PostFilter(IndexView):
+    
+    def get_queryset(self) :
+        author_pk = self.kwargs.get('author_pk')
+        author = models.Author.objects.get(id=author_pk)
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author=author)
+        return queryset
+        
+    
+
